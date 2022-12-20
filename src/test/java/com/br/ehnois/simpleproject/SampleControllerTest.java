43a03814c66d61b85796f40834f0d470e9b36297
Rafael Freire <rafaelcfreire@gmail.com>
2022-12-20 00:08:11 -0600

    broken test

---
+++ src/test/java/com/br/ehnois/simpleproject/SampleControllerTest.java
@@ -0,0 +1,21 @@
+package com.br.ehnois.simpleproject;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SampleControllerTest {
+
+    private SampleController sampleController = new SampleController();
+
+    @Test
+    public void testSampleController() {
+        assertEquals("Hello World!!", sampleController.getResponse());
+    }
+}
--- src/test/java/com/br/ehnois/simpleproject/SimpleProjectApplicationTests.java
+++ src/test/java/com/br/ehnois/simpleproject/SimpleProjectApplicationTests.java
@@ -8,6 +8,7 @@ class SimpleProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
