4f3c662efad02ac87b3949ca25cd227fc8bc06f7
Rafael Freire <rafaelcfreire@gmail.com>
2023-01-04 14:45:14 -0600

    Fixing unit test

--- src/test/java/com/br/ehnois/simpleproject/SampleControllerTest.java
+++ src/test/java/com/br/ehnois/simpleproject/SampleControllerTest.java
@@ -1,12 +1,8 @@
 package com.br.ehnois.simpleproject;
 
-import org.hamcrest.MatcherAssert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
